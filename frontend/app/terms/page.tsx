c40167254de003b9465deb836717faeba57fa40a
RazvanDRL <razdir97@gmail.com>
2024-04-06 21:28:51 +0300

    t&c

--- frontend/app/terms/page.tsx
+++ frontend/app/terms/page.tsx
@@ -2,7 +2,6 @@ export default function Terms() {
     return (
         <div>
             <div>
-
                 These terms of use were created using Termly's <a href="https://termly.io/products/terms-and-conditions-generator/">Terms and Conditions Generator</a>
             </div>
         </div>
