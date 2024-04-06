1198497738e6f1ce581513bc9fe1353321a83143
RazvanDRL <razdir97@gmail.com>
2024-04-06 21:30:46 +0300

    Fix typo in Terms component

--- frontend/app/terms/page.tsx
+++ frontend/app/terms/page.tsx
@@ -2,7 +2,7 @@ export default function Terms() {
     return (
         <div>
             <div>
-                These terms of use were created using Termly's <a href="https://termly.io/products/terms-and-conditions-generator/">Terms and Conditions Generator</a>
+                These terms of use were created using Termly&apos;s <a href="https://termly.io/products/terms-and-conditions-generator/">Terms and Conditions Generator</a>
             </div>
         </div>
     );
