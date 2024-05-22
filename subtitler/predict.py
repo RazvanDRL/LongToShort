1f500ad654818c3d621511c05da527648dd61128
RazvanDRL <razdir97@gmail.com>
2024-05-22 12:12:06 +0300

    revert

--- subtitler/predict.py
+++ subtitler/predict.py
@@ -11,7 +11,6 @@ import whisperx
 import json
 import subprocess
 import time
-import requests
 
 compute_type="float16"
 
@@ -47,16 +46,4 @@ class Predictor(BasePredictor):
             
             result = self.model.transcribe(str(f"{file}.wav"), batch_size=batch_size) 
             result = whisperx.align(result['segments'], self.alignment_model, self.metadata, str(f"{file}.wav"), self.device, return_char_alignments=False)
-        json_file_path = f"{str(file)}.json"
-        with open(json_file_path, 'w') as f:
-            json.dump(result['segments'], f)
-
-        with open(json_file_path, 'rb') as f:
-            response = requests.put("https://longtoshort.tech/api/upload"+"?key="+json_file_path, files={"file": f})
-
-        if response.status_code == 200:
-            print("File uploaded successfully.")
-        else:
-            print(f"Failed to upload file. Status code: {response.status_code}")
-
-        raise ValueError("ca00fccfb408989eddc401062c4d1219a6aceb6b9b55412357f1790862e8f178")
+        return json.dumps(result['segments'])
